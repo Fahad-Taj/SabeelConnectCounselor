fcc0e8dba6ff917e8745fa6989baef8e4492fb58
Fahad Taj <fahadtaj909@gmail.com>
2024-06-07 14:53:57 +0530

    Resolved the issue of bototm nav overlapping with navigation bar buttons

--- app/src/main/java/com/example/sabeelconnect/MainScreen.kt
+++ app/src/main/java/com/example/sabeelconnect/MainScreen.kt
@@ -7,10 +7,14 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.navigationBars
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.BottomNavigation
@@ -43,6 +47,7 @@ import com.example.sabeelconnect.ui.theme.PrimaryGreen
 fun MainScreen(rootNavController: NavHostController){
     val navController = rememberNavController()
     Scaffold(
+        modifier = Modifier.navigationBarsPadding(),
         bottomBar = { BottomBar(navController = navController) }
     ) {
         BottomNavGraph(rootNavController, navController, it)
@@ -66,6 +71,7 @@ fun BottomBar(navController: NavHostController){
             BottomNavigation(modifier = Modifier
                 .height(75.dp)
                 .background(Color.White)
+                .navigationBarsPadding(),
             ) {
                 screens.forEach{ screen ->
                     AddItem(screen = screen, currentDestination = currentDestination, navController = navController)
